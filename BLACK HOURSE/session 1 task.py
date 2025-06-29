04a24e3a86bbfa4bc99d6452e00b7e5a0400cc1b
LIGER37 <ahmed3731r@gmail.com>
2025-06-29 00:32:07 -0700

    Add files via upload

---
+++ BLACK HOURSE/session 1 task.py
@@ -0,0 +1,16 @@
+gtx1080= 8000
+rtx3050= 9000
+rtx4060 = 15000
+totalbudget =35000
+thetotalcost= gtx1080 + rtx3050 + rtx4060
+budgetsurplus= totalbudget - thetotalcost
+print("gtx1080" , gtx1080)
+print("rtx3050" , rtx3050)
+print('rtx4060' , rtx4060)
+print("totalbudget" , totalbudget)
+print("thetotalcost" , thetotalcost)
+if totalbudget > thetotalcost :
+    print("u can buy this products")
+else:
+    print("you can't buy this products")
+print("budgetsurplus:" , budgetsurplus)
---
+++ BLACK HOURSE/session 2 task 1.py
@@ -0,0 +1,9 @@
+temperature=25
+if temperature>=30:
+    print("it's a hot day.stay hydrated!")
+elif temperature>20 and temperature<30:
+    print("it's a warm day.enjoy the weather!")
+elif temperature>10 and temperature<20 :
+    print("it's a cool day.wear a jacket!")
+else :
+    print("it's a cold day.stay warm!")
